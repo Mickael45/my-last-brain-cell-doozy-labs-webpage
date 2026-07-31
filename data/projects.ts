4a722089be66ea370d5bb8f81d4a7496f0ace2db
Mickael <mickaelgomesconsulting@gmail.com>
2026-07-31 12:49:33 +0200

    Updates users number for JLAI

--- data/projects.ts
+++ data/projects.ts
@@ -306,7 +306,7 @@ export const projects: Project[] = [
       "impact": "Divided job search time by 2",
       "mrr": 0,
       "performance": "Summaries generated in under 3s",
-      "users": 12
+      "users": 16
     },
     "techStack": [
       "ReactJS",
